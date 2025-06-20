02ba0afe2352a648e06056500eff955f9e7ad140
HyangMok Baek <57baek@gmail.com>
2025-06-20 03:02:55 -0400

    3nd commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
     const yearSpan = document.getElementById("year");
     if (yearSpan) {
-      yearSpan.textContent = new Date().getFullYear();
+        yearSpan.textContent = new Date().getFullYear();
     }
-  });
+});
+
+let lastScrollTop = 0;
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+    const currentScroll =
+        window.pageYOffset || document.documentElement.scrollTop;
+
+    if (currentScroll > lastScrollTop) {
+        // scrolling down
+        header.classList.add("hide");
+    } else {
+        // scrolling up
+        header.classList.remove("hide");
+    }
+
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+});
